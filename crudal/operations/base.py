40dc37c037d7910a3ec4dc05c0aab9dbbd1f5d1e
andrewsap <andrewsapw@gmail.com>
2023-03-20 22:44:33 +0300

    feat: filter return fields in select statement

--- crudal/operations/base.py
+++ crudal/operations/base.py
@@ -1,2 +1,12 @@
-from typing import Protocol
+import typing as t
 
+from sqlalchemy import select
+
+
+def _select_stmt_fields(fields):
+    if isinstance(t, tuple) or isinstance(t, list):
+        stmt = select(*fields)
+    else:
+        stmt = select(fields)
+
+    return stmt
