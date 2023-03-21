cb90503965dd6e7e20f8a2633634e18cdcc12e06
andrewsap <andrewsapw@gmail.com>
2023-03-21 13:29:13 +0300

    refactor: CRUDALType as Protocol for typing help

---
+++ crudal/types.py
@@ -0,0 +1,40 @@
+import typing as t
+
+from sqlalchemy.orm import Session
+
+_T = t.TypeVar("_T")
+
+
+class CRUDALType(t.Protocol):
+    @classmethod
+    def _get_primary_key(cls) -> str:
+        """Table primary key"""
+        raise NotImplementedError()
+
+    @classmethod
+    def find(cls: t.Type[_T], session: Session, **filters) -> t.Sequence[_T]:
+        """Find items"""
+        raise NotImplementedError()
+
+    @classmethod
+    def find_by_pk(cls: t.Type[_T], pk: t.Any, session: Session) -> t.Optional[_T]:
+        raise NotImplementedError()
+
+    @classmethod
+    def exists(cls, session: Session, **filters) -> bool:
+        raise NotImplementedError()
+
+    @classmethod
+    def all(cls: t.Type[_T], session: Session) -> t.Sequence[_T]:
+        raise NotImplementedError()
+
+    @classmethod
+    def delete(cls, session: Session, **filters) -> bool:
+        raise NotImplementedError()
+
+    @classmethod
+    def add_many(cls: t.Type[_T], session: Session, items: t.List[_T]) -> None:
+        raise NotImplementedError()
+
+    def add(self, session: Session, commit: bool = False) -> None:
+        raise NotImplementedError()
