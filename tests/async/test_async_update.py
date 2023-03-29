7733e160b619edcef2aea794a1f7637471f1361b
andrewsap <andrewsapw@gmail.com>
2023-03-29 21:21:31 +0300

    tests: fixed async update test

--- tests/async/test_async_update.py
+++ tests/async/test_async_update.py
@@ -7,17 +7,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 async def test_async_update():
     async with AsyncSession(bind=async_engine) as async_session:
         p = PersonAsync(name="AndrewSON")
-        p = await p.add(session=async_session, commit=True)
+        p = await p.add(async_session, commit=True)
 
-        assert p in (await p.all(session=async_session))
+        assert p in (await p.all(async_session))
 
         new_name = "AndrewSON new Name"
         new_values = dict(name=new_name)
-        await PersonAsync.update(
-            session=async_session, values=new_values, name="AndrewSON"
-        )
+        await PersonAsync.update(async_session, values=new_values, name="AndrewSON")
 
-        p_new = await PersonAsync.find(session=async_session, name=new_name)
+        p_new = await PersonAsync.find(async_session, name=new_name)
 
         assert len(p_new) == 1
         assert p.id == p_new[0].id
