05a668e3169630160733ffde092da063ba9bbcf6
Andrey <a.saparov@click-group.ru>
2023-03-30 15:59:06 +0300

    tests: fix session argument

--- tests/sync_tests/test_basic.py
+++ tests/sync_tests/test_basic.py
@@ -5,11 +5,11 @@ from crudal.base_sync import DeclarativeCrudBase
 
 def test_add(sync_model, session: Session):
     p = sync_model(name="Andrew")
-    p.add(session=session, commit=True)
+    p.add(session, commit=True)
 
-    assert p in p.all(session=session)
-    assert p in p.find(session=session, name="Andrew")
-    assert p not in p.find(session=session, name="Not Andrew")
+    assert p in p.all(session)
+    assert p in p.find(session, name="Andrew")
+    assert p not in p.find(session, name="Not Andrew")
 
 
 def test_delete(sync_model: DeclarativeCrudBase, session: Session):
--- tests/sync_tests/test_update.py
+++ tests/sync_tests/test_update.py
@@ -3,15 +3,15 @@ from sqlalchemy.orm import Session
 
 def test_update(sync_model, session: Session, random_string):
     p = sync_model(name="AndrewSON")
-    p = p.add(session=session, commit=True)
+    p = p.add(session, commit=True)
 
-    assert p in p.all(session=session)
+    assert p in p.all(session)
 
     new_name = random_string
     new_values = dict(name=new_name)
-    sync_model.update(session=session, values=new_values, name="AndrewSON")
+    sync_model.update(session, values=new_values, name="AndrewSON")
 
-    p_new = sync_model.find(session=session, name=new_name)
+    p_new = sync_model.find(session, name=new_name)
 
     assert len(p_new) == 1
     assert p.id == p_new[0].id
